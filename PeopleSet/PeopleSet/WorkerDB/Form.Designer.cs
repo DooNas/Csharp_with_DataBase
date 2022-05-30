d32f3706deb33faf8954f077173ab9eca988c9f1
DooNas <jaejoon4929@gmail.com>
2022-05-30 23:26:48 +0900

    직원관리 CRUD 프로그램

---
+++ PeopleSet/PeopleSet/WorkerDB/Form.Designer.cs
@@ -0,0 +1,420 @@
+﻿
+namespace WindowsFormsDB
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtFlbValue = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtFlbType = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtFlaValue = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtFlaType = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtJob = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDept = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.myComDataGridView = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.myComDataGridView)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnInsert);
+            this.panel1.Controls.Add(this.txtSalary);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.txtScore);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.txtFlbValue);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.txtFlbType);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.txtFlaValue);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.txtFlaType);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtLevel);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtJob);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtDept);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtId);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 185);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(694, 88);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(98, 34);
+            this.btnDelete.TabIndex = 26;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Location = new System.Drawing.Point(694, 48);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(98, 34);
+            this.btnUpdate.TabIndex = 25;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInsert.Location = new System.Drawing.Point(694, 7);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(98, 34);
+            this.btnInsert.TabIndex = 24;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(254, 145);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(100, 21);
+            this.txtSalary.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(220, 149);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(29, 12);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "연봉";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(115, 145);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 21);
+            this.txtScore.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 149);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(97, 12);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "차년도 업적 등급";
+            // 
+            // txtFlbValue
+            // 
+            this.txtFlbValue.Location = new System.Drawing.Point(559, 99);
+            this.txtFlbValue.Name = "txtFlbValue";
+            this.txtFlbValue.Size = new System.Drawing.Size(100, 21);
+            this.txtFlbValue.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(466, 103);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(87, 12);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "제2외국어 점수";
+            // 
+            // txtFlbType
+            // 
+            this.txtFlbType.Location = new System.Drawing.Point(360, 99);
+            this.txtFlbType.Name = "txtFlbType";
+            this.txtFlbType.Size = new System.Drawing.Size(100, 21);
+            this.txtFlbType.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(295, 103);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 12);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "제2외국어";
+            // 
+            // txtFlaValue
+            // 
+            this.txtFlaValue.Location = new System.Drawing.Point(189, 99);
+            this.txtFlaValue.Name = "txtFlaValue";
+            this.txtFlaValue.Size = new System.Drawing.Size(100, 21);
+            this.txtFlaValue.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(154, 103);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "점수";
+            // 
+            // txtFlaType
+            // 
+            this.txtFlaType.Location = new System.Drawing.Point(48, 99);
+            this.txtFlaType.Name = "txtFlaType";
+            this.txtFlaType.Size = new System.Drawing.Size(100, 21);
+            this.txtFlaType.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "영어";
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.Location = new System.Drawing.Point(389, 55);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(100, 21);
+            this.txtLevel.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(344, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "직급";
+            // 
+            // txtJob
+            // 
+            this.txtJob.Location = new System.Drawing.Point(225, 54);
+            this.txtJob.Name = "txtJob";
+            this.txtJob.Size = new System.Drawing.Size(100, 21);
+            this.txtJob.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(180, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "직군";
+            // 
+            // txtDept
+            // 
+            this.txtDept.Location = new System.Drawing.Point(58, 53);
+            this.txtDept.Name = "txtDept";
+            this.txtDept.Size = new System.Drawing.Size(100, 21);
+            this.txtDept.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "부서";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(389, 13);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 21);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(344, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "이메일";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(225, 14);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 21);
+            this.txtName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "이름";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(58, 13);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 21);
+            this.txtId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.myComDataGridView);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 185);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.panel2.Size = new System.Drawing.Size(800, 230);
+            this.panel2.TabIndex = 1;
+            // 
+            // myComDataGridView
+            // 
+            this.myComDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.myComDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myComDataGridView.Location = new System.Drawing.Point(10, 0);
+            this.myComDataGridView.Name = "myComDataGridView";
+            this.myComDataGridView.RowTemplate.Height = 23;
+            this.myComDataGridView.Size = new System.Drawing.Size(780, 230);
+            this.myComDataGridView.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnSearch);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 415);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 45);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Location = new System.Drawing.Point(695, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(98, 34);
+            this.btnSearch.TabIndex = 25;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 460);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form4";
+            this.Text = "직원관리 시스템";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.myComDataGridView)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtFlbValue;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtFlbType;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtFlaValue;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtFlaType;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtJob;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDept;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView myComDataGridView;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
---
+++ PeopleSet/PeopleSet/WorkerDB/Form.cs
@@ -0,0 +1,172 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDB
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            btnInsert.Click += BtnInsert_Click;
+            btnSearch.Click += BtnSearch_Click;
+            myComDataGridView.CellClick += MyComDataGridView_CellClick;
+            btnUpdate.Click += BtnUpdate_Click;
+            btnDelete.Click += BtnDelete_Click;
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(mySqlConnStr);
+                conn.Open();
+                StringBuilder sb = new StringBuilder();
+                sb.Append(" delete from `MYCOMPANY1`.`mycompany1`");
+                sb.Append(" where ");
+                sb.Append(" id = '"); sb.Append(txtId.Text); sb.Append("' ");
+                string sql = sb.ToString();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                Search();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void MyComDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //e.RowIndex
+            string id = myComDataGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
+            string name = myComDataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            string email = myComDataGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            string dept = myComDataGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
+            string job = myComDataGridView.Rows[e.RowIndex].Cells[4].Value.ToString();
+            string level = myComDataGridView.Rows[e.RowIndex].Cells[5].Value.ToString();
+            string flaType = myComDataGridView.Rows[e.RowIndex].Cells[6].Value.ToString();
+            string flaValue = myComDataGridView.Rows[e.RowIndex].Cells[7].Value.ToString();
+            string flbType = myComDataGridView.Rows[e.RowIndex].Cells[8].Value.ToString();
+            string flbValue = myComDataGridView.Rows[e.RowIndex].Cells[9].Value.ToString();
+            string score = myComDataGridView.Rows[e.RowIndex].Cells[10].Value.ToString();
+            string salary = myComDataGridView.Rows[e.RowIndex].Cells[11].Value.ToString();
+            txtId.Text = id;
+            txtName.Text = name;
+            txtEmail.Text = email;
+            txtDept.Text = dept;
+            txtJob.Text = job;
+            txtLevel.Text = level;
+            txtFlaType.Text = flaType;
+            txtFlaValue.Text = flaValue;
+            txtFlbType.Text = flbType;
+            txtFlbValue.Text = flbValue;
+            txtScore.Text = score;
+            txtSalary.Text = salary;
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(mySqlConnStr);
+                conn.Open();
+                StringBuilder sb = new StringBuilder();
+                sb.Append(" update `MYCOMPANY1`.`mycompany1`");
+                sb.Append(" set ");
+                sb.Append(" `name` = '"); sb.Append(txtName.Text); sb.Append("', ");
+                sb.Append(" `email` = '"); sb.Append(txtEmail.Text); sb.Append("', ");
+                sb.Append(" `dept` = '"); sb.Append(txtDept.Text); sb.Append("', ");
+                sb.Append(" `job` = '"); sb.Append(txtJob.Text); sb.Append("', ");
+                sb.Append(" `level` = '"); sb.Append(txtLevel.Text); sb.Append("', ");
+                sb.Append(" `flatype` = '"); sb.Append(txtFlaType.Text); sb.Append("', ");
+                sb.Append(" `flavalue` = '"); sb.Append(txtFlaValue.Text); sb.Append("', ");
+                sb.Append(" `flbtype` = '"); sb.Append(txtFlbType.Text); sb.Append("', ");
+                sb.Append(" `flbvalue` = '"); sb.Append(txtFlbValue.Text); sb.Append("', ");
+                sb.Append(" `score` = '"); sb.Append(txtScore.Text); sb.Append("', ");
+                sb.Append(" salary = '"); sb.Append(txtSalary.Text); sb.Append("' ");
+                sb.Append(" where ");
+                sb.Append(" id = '"); sb.Append(txtId.Text); sb.Append("' ");
+
+                string sql = sb.ToString();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                Search();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        private static string mySqlConnStr = DB_Key.GetKey();
+        DataSet ds;
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            //데이터 조회를 위해서..
+            Search();
+        }
+
+        private void Search()
+        {
+            try
+            {
+                ds = new DataSet();
+                MySqlConnection conn = new MySqlConnection(mySqlConnStr);
+                conn.Open();
+                string sql = "select * from MYCOMPANY1.mycompany1";
+                //쿼리와 커넥션으로 쿼리 실행
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                //데이터 채우고
+                adapter.Fill(ds, "mycompany1");
+                //채워진 데이터를 데이터그리드뷰에 할당
+                myComDataGridView.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(mySqlConnStr);
+                conn.Open();
+                StringBuilder sb = new StringBuilder();
+                sb.Append(" insert into `MYCOMPANY1`.`mycompany1` ");
+                sb.Append(" values ( ");
+                sb.Append(" '");sb.Append(txtId.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtName.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtEmail.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtDept.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtJob.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtLevel.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtFlaType.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtFlaValue.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtFlbType.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtFlbValue.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtScore.Text); sb.Append("', ");
+                sb.Append(" '"); sb.Append(txtSalary.Text); sb.Append("' ");
+                sb.Append(" ) ");
+                string sql = sb.ToString();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                Search();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
